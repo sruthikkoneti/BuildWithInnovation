f6565445ff8742dad95f5c7d4675f3bc481d595f
Sruthik Koneti <sruthikkoneti@gmail.com>
2023-12-31 20:21:17 +0530

    update user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,3 +60,21 @@ export const deleteAccount = async (req, res) => {
     return res.status(500).json({ success: false, message: 'Failed to delete account.' });
   }
 };
+
+
+export const getUserDetails = async (req, res) => {
+  try {
+    const { userID } = req.user_id; // Assuming userId is sent as a route parameter
+
+    // Find the user by ID
+    const user = await User.findById(userID);
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found.' });
+    }
+    
+    return res.status(200).json({ success: true, user });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: 'Failed to fetch user details.' });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { auth } from "../middleware/auth.js"
 import { updateUpload } from "../middleware/updateUpload.js"
-import { deleteAccount, updateUserProfile } from "../controllers/userController.js"
+import { deleteAccount, getUserDetails, updateUserProfile } from "../controllers/userController.js"
 
 const router = express.Router()
 
 router.patch("/update",[auth,updateUpload],updateUserProfile)
 router.delete("/delete",auth,deleteAccount)
+router.get("/get",auth,getUserDetails)
 
 export default router
